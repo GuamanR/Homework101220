9dc6906b1bd1ab5f87068fac6f5df9f665e22e20
Richie <richie.guaman@gmail.com>
2020-10-12 17:57:19 -0400

    updated magic calculator

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,5 @@
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable.UnaryOp.Sqrt;
+
 public class Calculator {
     public int add(int num1, int num2){
         return num1+num2;
@@ -18,5 +20,28 @@ public class Calculator {
             num= num1*num;
         }
         return num; 
+    }
+    public class MagicCalculator{
+        // Double just means that the computer will take double digits aka whole numbers and decimals
+        public double root(double num1){
+            return Math.sqrt(num1);
+        }
+        public double sin(double num1){
+            return Math.sin(num1);
+        }
+        public double cos(double num1){
+            return Math.cos(num1);
+        }
+        public double tan(double num1){
+            return Math.tan(num1);
+        }
+        public int fact(int num1){
+            int num = num1;
+            for (int i=num1-1; i>1; i--){
+                num= num*i;
+            }
+            return num; 
+
+        }
     }
 }
